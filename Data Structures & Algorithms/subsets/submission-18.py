1a378d88c1b19a5d9f68d071f526f5d1b1538cd9
Sean Luu <seanluu3@gmail.com>
2026-09-17 17:37:28 -0400

    Add: subsets - submission-18

---
+++ Data Structures & Algorithms/subsets/submission-18.py
@@ -0,0 +1,28 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        
+        # base case: if i (path) >= len(nums) 
+        # choices: include nums[i] in subset or exclude nums[i] from subset
+        # constraints: i (path) cannot go out of bounds 
+        # backtracking step: pop the last added element to the subset
+
+        res = []
+        subset = []
+
+        def dfs(i):  # i = index we're deciding in/out for
+            if i >= len(nums):
+                # base case + bounds constraint (same condition, does both):
+                # no more indices to decide on & i is no longer valid to index with
+                res.append(subset.copy())
+                return
+            
+            # choice #1: include nums[i] in the subset
+            subset.append(nums[i])
+            dfs(i + 1) # increment in index once we append to our subset
+            subset.pop() # backtracking step
+
+            # choice #2: exclude nums[i] from the subset
+            dfs(i + 1) # increment in index, despite not doing anything (excluding nums[i])
+        dfs(0) # start at origin
+    
+        return res
