1be97345b3322a87058b0b8eff13a0b90f9bca98
Sean Luu <seanluu3@gmail.com>
2026-09-18 14:03:36 -0700

    Add: generate-parentheses - submission-20

---
+++ Data Structures & Algorithms/generate-parentheses/submission-20.py
@@ -0,0 +1,26 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        res = []
+
+        stack = []
+
+        def dfs(openN, closedN):
+            if openN == n == closedN: # if number of open and closed parenthesis = number of n pairs, continue
+                res.append("".join(stack)) 
+                return
+
+            if openN < n:
+                stack.append("(")
+                dfs(openN + 1, closedN)
+                stack.pop()
+
+            if openN > closedN:
+                stack.append(")")
+                dfs(openN, closedN + 1)
+                stack.pop()
+
+        dfs(0, 0)
+
+        return res
+
