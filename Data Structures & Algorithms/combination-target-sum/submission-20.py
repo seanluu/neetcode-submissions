e593f425f7a603e03fe4e211ccf1167d96c8fd21
Sean Luu <seanluu3@gmail.com>
2026-09-22 00:56:50 -0700

    Add: combination-target-sum - submission-20

---
+++ Data Structures & Algorithms/combination-target-sum/submission-20.py
@@ -0,0 +1,31 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+        # base case: if total == target, stop here
+        # choices: either include nums[i] in comb sum or exclude nums[i] from it
+        # constraints: total cannot exceed target and i cannot go out of bounds
+        # backtracking step: pop the last added element
+
+        res = []
+
+        # index, current path, total
+        def dfs(i, curr, total):
+            if total == target: # base case
+                res.append(curr.copy())
+                return
+
+            # constraints
+            if total > target or i >= len(nums):
+                return
+
+            # choice #1: include nums[i]
+            curr.append(nums[i])
+            dfs(i, curr, total + nums[i])
+            curr.pop()
+
+            # choice #2: exclude nums[i]
+            dfs(i + 1, curr, total)
+
+        dfs(0, [], 0) # set every parameter at origin
+
+        return res
